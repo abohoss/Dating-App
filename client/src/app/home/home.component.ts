ee8c19d0fc7d1dc12f9154aad4f62258489f08b7
abohoss <21p0141@eng.asu.edu.eg>
2024-08-31 15:55:57 +0300

    login

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  http = inject(HttpClient);
+  users: any;
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/user/').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('get request completed')
+    })
+  }
+}
